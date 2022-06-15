c99e8d058ec519acd66af27f0a4aa0847d520569
Daniel Attias <danielattias@live.com>
2022-06-14 22:07:06 -0400

    atualizações

--- modulo02/aula06/index.php
+++ modulo02/aula06/index.php
@@ -1,7 +1,18 @@
 <?php
 
-include 'classes/.php';
-include 'classes/.php';
-include 'classes/.php';
-include 'classes/.php';
-include 'classes/.php';
+// include 'classes/Usuario.php';
+// include 'classes/Cliente.php';
+// include 'classes/Gestor.php';
+// include 'classes/Produto.php';
+// include 'classes/Categoria.php';
+// include 'classes/Config/Usuario.php'
+
+require 'vendor/autoload.php';
+
+use Classes\Config\Usuario as UsuarioConfig;
+
+$us1 = new Classes\Usuario();
+$us2 = new UsuarioConfig();
+
+var_dump($us1);
+var_dump($us2);
